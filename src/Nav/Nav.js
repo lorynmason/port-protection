c1a6a62dcd0e5bedcd422eacc685a0e612b093f0
Loryn Mason <masonloryn@gmail.com>
2019-04-14 15:48:02 -0700

    Add Nav component

---
+++ src/Nav/Nav.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export const Nav = () => {
+  return (
+    <nav>
+      <button className="nav-button">Lodging</button>
+      <button className="nav-button">Skiff Rentals</button>
+      <button className="nav-button">Other Service</button>
+      <button className="nav-button">Contact Us</button>
+      <button className="nav-button">About</button>
+    </nav>
+  )
+}
