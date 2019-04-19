7e554358bb6185bb2008b873ae3dfc0b19394245
Loryn Mason <masonloryn@gmail.com>
2019-04-19 14:42:41 -0700

    Add NavLinks to Nav buttons

--- src/Nav/Nav.js
+++ src/Nav/Nav.js
@@ -1,13 +1,24 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom'
 
 export const Nav = () => {
   return (
     <nav>
-      <button className="nav-button">Lodging</button>
-      <button className="nav-button">Skiff Rentals</button>
-      <button className="nav-button">Other Service</button>
-      <button className="nav-button">Contact Us</button>
-      <button className="nav-button">About</button>
+      <NavLink to="/lodging">
+        <button className="nav-button">Lodging</button>
+      </NavLink>
+      <NavLink to="/skiff-rentals">
+        <button className="nav-button">Skiff Rentals</button>
+      </NavLink>
+      <NavLink to="/other-services">
+        <button className="nav-button">Other Services</button>
+      </NavLink>
+      <NavLink to="/contact">
+        <button className="nav-button">Contact Us</button>
+      </NavLink>
+      <NavLink to="/about">
+        <button className="nav-button">About</button>
+      </NavLink>
     </nav>
   )
 }
