1db2ff55652b7150842a8001d4c05b5764309c85
Loryn Mason <masonloryn@gmail.com>
2019-04-19 14:31:23 -0700

    Setup home component to make routing easier

--- src/App/App.js
+++ src/App/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import '../styles/main.scss';
-import lodgeLogo from "../styles/images/image.png";
 import { Nav } from "../Nav/Nav";
+import { Home } from "../Home/Home"
 import { Route, Switch } from 'react-router-dom'
 
 class App extends Component {
@@ -9,10 +9,7 @@ class App extends Component {
     return (
       <div className="App">
         <Nav />
-        <div id="lodge-logo">
-          <img src={lodgeLogo} alt="Port Protection Lodge Logo"/>
-          <h1>A True Alaskan Adventure</h1>
-        </div>
+        <Home />
         <video id="background-video"  loop autoPlay muted>
           <source src="https://videos.files.wordpress.com/J5vpit3s/video-feb-24-10-49-36-am_hd.mp4" type="video/mp4" />
           Your browser does not support the video tag.
---
+++ src/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import lodgeLogo from "../styles/images/image.png";
+
+
+export const Home = () => {
+  return (
+    <div id="lodge-logo">
+      <img src={lodgeLogo} alt="Port Protection Lodge Logo"/>
+      <h1>A True Alaskan Adventure</h1>
+    </div>
+  )
+}
