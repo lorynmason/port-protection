f49578c90080bf4409ce40be009e44f1a27577fd
Loryn Mason <masonloryn@gmail.com>
2019-04-26 17:56:40 -0700

    Add link to home in nav logo, edit home logo

--- src/Nav/Nav.js
+++ src/Nav/Nav.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, Link } from 'react-router-dom';
 import lodgeLogo from "../styles/images/image.png";
 
 export const Nav = () => {
   return (
     <nav>
-      <img src={lodgeLogo} alt="Port Protection Lodge Logo" id="small-logo"/>
+      <Link to="/"><img src={lodgeLogo} alt="Port Protection Lodge Logo" id="small-logo"/></Link>
       <NavLink to="/lodging">
         <button className="nav-button">Lodging</button>
       </NavLink>
