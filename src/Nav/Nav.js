f758776fe3a0d9e727a4240bed2f426f2f11d383
Loryn Mason <masonloryn@gmail.com>
2019-04-26 17:29:55 -0700

    Add Header for navigation

--- src/Nav/Nav.js
+++ src/Nav/Nav.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom'
+import { NavLink } from 'react-router-dom';
+import lodgeLogo from "../styles/images/image.png";
 
 export const Nav = () => {
   return (
     <nav>
+      <img src={lodgeLogo} alt="Port Protection Lodge Logo" id="small-logo"/>
       <NavLink to="/lodging">
         <button className="nav-button">Lodging</button>
       </NavLink>
@@ -12,13 +14,13 @@ export const Nav = () => {
       </NavLink>
       <NavLink to="/other-services">
         <button className="nav-button">Other Services</button>
+      </NavLink> 
+      <NavLink to="/about">
+        <button className="nav-button">About</button>
       </NavLink>
       <a href="https://portprotectionlodge.typeform.com/to/EgWbs7">
         <button className="nav-button">Contact Us</button>
       </a>
-      <NavLink to="/about">
-        <button className="nav-button">About</button>
-      </NavLink>
     </nav>
   )
 }
